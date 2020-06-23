53cea8ca2d8c73f5a745578a0206401524c218d0
Bayu Tudo Prasetyo <48831713+1700018096@users.noreply.github.com>
2020-06-23 22:03:31 +0800

    Add files via upload

---
+++ form1.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+	Selamat datang : <?php echo $_POST["dname"]; ?> <?php echo $_POST["bname"]; ?><br>
+	Prestasi anda : <?php echo $_POST["Prestasi"]; ?><br>
+	Hobi anda : <?php echo $_POST["hobi"]; ?><br>
+	Agama anda : <?php echo $_POST["agama"]; ?><br>
+
+
+
+</body>
+</html>
