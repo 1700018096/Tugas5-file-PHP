888d8a608c0cba7e3227d3ff2a07df70d4b96f16
Bayu Tudo Prasetyo <48831713+1700018096@users.noreply.github.com>
2020-07-13 09:40:44 +0800

    Update form1.php

--- form1.php
+++ form1.php
@@ -4,12 +4,37 @@
 	<title></title>
 </head>
 <body>
+	<style>
+		body {
+			background-image: url("salju.jpg");
+			font-style: 16px sans-serif;
+			color: white;
+			text-align: center;
+		}
+	</style>
+	<center>
+		<?php 
+		echo "<head><title>Form Project</head></title>"; 
+	$fp = fopen("form.txt", "a+");
+	$dname = $_POST["dname"];
+	$bname = $_POST["bname"];
+	$Prestasi = $_POST["Prestasi"];
+	$hobi = $_POST["hobi"];
+	$agama = $_POST["agama"];
+	
+	fputs($fp, "$dname|$bname|$Prestasi|$hobi|$agama\n");
+	fclose($fp);
+
+		?>
 	Selamat datang : <?php echo $_POST["dname"]; ?> <?php echo $_POST["bname"]; ?><br>
 	Prestasi anda : <?php echo $_POST["Prestasi"]; ?><br>
 	Hobi anda : <?php echo $_POST["hobi"]; ?><br>
 	Agama anda : <?php echo $_POST["agama"]; ?><br>
-
-
+		
+		<br><a href="form.html">Menu Form</a><br>
+		<a href="tabel.html">Tabel Html</a>
+		<a href="form.txt">Isi Form txt</a>
+	</center>
 
 </body>
 </html>
